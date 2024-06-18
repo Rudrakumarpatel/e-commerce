da6ec87b2c854e89c812fe41470f2dbd5dbafbba
Rudrakumar Patel <rudrapatel2992003@gmail.com>
2024-06-18 12:19:47 +0530

    update

--- model/User.js
+++ model/User.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: Buffer, required: true },
-  role: { type: String, required: true, default:'user' },
+  role: { type: String, required: true, default:'admin' },
   addresses: { type: [Schema.Types.Mixed] }, 
   // for addresses, we can make a separate Schema like orders. but in this case we are fine
   name: { type: String },
